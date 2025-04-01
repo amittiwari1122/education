0bdf17e894d31f5a963f9b17f4b13072c998cfb6
Amit tiwari <infoindian.amit@gmail.com>
2025-04-01 12:12:20 +0530

    add mobile

--- resources/views/email/contact.blade.php
+++ resources/views/email/contact.blade.php
@@ -7,7 +7,9 @@
 
 <body>
     <h2>New Message from {{ $data['name'] }}</h2>
+    <p><strong>Name:</strong> {{ $data['name'] }}</p>
     <p><strong>Email:</strong> {{ $data['email'] }}</p>
+    <p><strong>Contact:</strong> {{ $data['contact'] }}</p>
     <p><strong>Message:</strong></p>
     <p>{{ $data['message'] }}</p>
     <p>Thank you!</p>
